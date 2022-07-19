de04b2fa9978d65498529f8b7d819e1d8a404238
MuktiIngsun <107071706+MuktiIngsun@users.noreply.github.com>
2022-07-19 15:21:40 +0000

    Update GetAudioYT.sh

--- GetAudioYT.sh
+++ GetAudioYT.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # locate YT downloader
-source /home/mukti-ingsun/Documents/PyYt/bin/activate
+# if you use python env, uncomment the code below
+# source /YourPythonPath/bin/activate 
 python3 /home/mukti-ingsun/Documents/PyYt/get-audio-yt.py
 ffmpeg -i "$(ls | grep .webm)" "$(ls | sed -n 's/\.webm$//p')".mp3
 rm *.webm
-mv *.mp3 /home/mukti-ingsun/Music 
+mv *.mp3 /YourDirectory 

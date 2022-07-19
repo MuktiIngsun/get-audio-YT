4816b19b1bfd118b52212ad52569865f0f581f3c
MuktiIngsun <107071706+MuktiIngsun@users.noreply.github.com>
2022-07-19 15:15:30 +0000

    Update get-audio-yt.py

--- get-audio-yt.py
+++ get-audio-yt.py
@@ -3,12 +3,13 @@ from pytube import YouTube
 url = input("YT Link: ")
 yt = YouTube(url)
 
-print("*********************Video Title************************")
+print("===Video Title===")
 #get Video Title
 print(yt.title)
 
+# filter audio tag
 yt.streams.filter(only_audio=True)
 
-print("********************Download video*************************")
-stream = yt.streams.get_by_itag(251)
+print("===Download Audio===")
+stream = yt.streams.get_by_itag(251) # 251 is the audio tag I chose
 stream.download()
